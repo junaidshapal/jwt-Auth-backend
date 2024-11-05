d819e2f833b7d842cd99ea7bb15722189455f102
Junaid <mughaljunaidmughal136@gmail.com>
2024-11-05 20:52:55 +0500

    Validations

--- JwtAuthentication/Models/LoginViewModel.cs
+++ JwtAuthentication/Models/LoginViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace JwtAuthentication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthentication.Models
 {
     public class LoginViewModel
     {
+        [Required]
         public string UserName { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = string.Empty;
     }
 }
